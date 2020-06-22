795199221f9302ea0f9727cf953f47bfc4356d22
Jason <jason.rostar@yahoo.com>
2020-06-22 10:15:40 -0700

    added login page with private routes

--- src/App.js
+++ src/App.js
@@ -1,56 +1,29 @@
-import React from 'react';
-import { useEffect, useState } from 'react';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+import PrivateRoute from './components/PrivateRoute';
+import Login from "./components/Login";
+
+import Dashboard from './components/Dashboard'
 
 function App() {
-  const [date, setDate] = useState(null);
-  useEffect(() => {
-    async function getDate() {
-      const res = await fetch('/api/date');
-      const newDate = await res.text();
-      setDate(newDate);
-    }
-    getDate();
-  }, []);
   return (
-    <main>
-      <h1>Create React App + Go API</h1>
-      <h2>
-        Deployed with{' '}
-        <a
-          href="https://vercel.com/docs"
-          target="_blank"
-          rel="noreferrer noopener"
-        >
-          Vercel
-        </a>
-        !
-      </h2>
-      <p>
-        <a
-          href="https://github.com/vercel/vercel/tree/master/examples/create-react-app"
-          target="_blank"
-          rel="noreferrer noopener"
-        >
-          This project
-        </a>{' '}
-        was bootstrapped with{' '}
-        <a href="https://facebook.github.io/create-react-app/">
-          Create React App
-        </a>{' '}
-        and contains three directories, <code>/public</code> for static assets,{' '}
-        <code>/src</code> for components and content, and <code>/api</code>{' '}
-        which contains a serverless <a href="https://golang.org/">Go</a>{' '}
-        function. See{' '}
-        <a href="/api/date">
-          <code>api/date</code> for the Date API with Go
-        </a>
-        .
-      </p>
-      <br />
-      <h2>The date according to Go is:</h2>
-      <p>{date ? date : 'Loading date...'}</p>
-    </main>
+    <Router>
+     <div>
+        <ul>
+            <li>
+                <Link to='/login'>Login</Link>
+            </li>
+            <li>
+                <Link to='/protected'>Dashboard</Link>
+            </li>
+        </ul>
+        <Switch>
+            <PrivateRoute exact path='/protected' component={Dashboard}/>
+            <Route path='/login' component={Login}/>
+            <Route component={Login}/>
+        </Switch>
+    </div>
+    </Router>
   );
 }
 
